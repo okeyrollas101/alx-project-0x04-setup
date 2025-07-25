77d340cfc08a43b48cef88398c0fb53fb5837774
Mac <mac@okey101.local>
2025-07-25 23:51:31 +0100

    Fixed pages/index.tsx error again

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -2,19 +2,19 @@ import Button from "@/components/common/Button";
 import { useRouter } from "next/router";
 
 interface PageRouteProps {
-   pageRoute: string;
+  pageRoute: string
 }
 
+export default function Home() {
+  const router = useRouter()
 
-export default function Home () {
-  const router = useRouter();
-  // Imperative routing with useRouter
-  const routeToNextPage = ({pageRoute}: PageRouteProps) => {
-     router.push(pageRoute, undefined, { shallow: false });
-  };
+  // Imeperative routing with useRouter
+  const routeToNextPage  = ({ pageRoute }: PageRouteProps) => {
+    router.push(pageRoute, undefined, { shallow: false})
+  }
 
   return (
-   <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
       {/* Welcome Message */}
       <h1 className="text-4xl font-bold text-gray-800 mb-4">
         Welcome to Splash App!
