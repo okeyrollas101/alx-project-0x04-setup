3ea3334c89155891bdb7dc530c861a2de803b6f1
Mac <mac@okey101.local>
2025-07-25 23:47:28 +0100

    Fixed pages/index.tsx error

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -6,7 +6,7 @@ interface PageRouteProps {
 }
 
 
-const Home = () => {
+export default function Home () {
   const router = useRouter();
   // Imperative routing with useRouter
   const routeToNextPage = ({pageRoute}: PageRouteProps) => {
@@ -33,5 +33,3 @@ const Home = () => {
     </div>
   );
 }
-
-export default Home;
